bec24d9847fba229840f0c564d28474b9ed2ce0f
MDASFAQ08git <mdasfaq0808@gmail.com>
2025-12-29 13:49:00 +0530

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var typed = new Typed(".role",{
-    strings:["Frontend Developer","Backend Developer"],
-    typeSpeed:100,
+    strings:["Frontend Developer","Backend Developer","Java Developer"],
+    typeSpeed:50,
     backSpeed:100,
     backDelay:1000,
     loop:true
